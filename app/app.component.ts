772efcc91072db2491263695ee147ffe3f127f52
Guest User <Guest@epicodus-94.local>
2018-01-16 15:13:36 -0800

    use ngIf to hide edit form

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,27 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container">
+  <div class="container">
       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
       <h3>{{currentFocus}}</h3>
       <ul>
-        <li [class]="priorityColor(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+        <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
       </ul>
       <hr>
       <div>
-        <h3>{{selectedTask.description}}</h3>
-        <p>Task Complete? {{selectedTask.done}}</p>
-        <h3>Edit Task</h3>
-        <div class="form-group">
+        <div *ngIf="selectedTask">
+          <h3>{{selectedTask.description}}</h3>
+          <p>Task Complete? {{selectedTask.done}}</p>
+          <hr>
+          <h3>Edit Task</h3>
           <label>Enter Task Description:</label>
           <input [(ngModel)]="selectedTask.description" class="form-control">
+          <br>
+          <label>Enter Task Priority (1-3):</label><br>
+          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+          <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+          <br>
+          <button (click)="finishedEditing()">Done</button>
         </div>
-        <br>
-        <label>Enter Task Priority (1-3):</label>
-        <br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1"> 1 (Low Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2"> 2 (Medium Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3"> 3 (High Priority)
       </div>
     </div>
   `
@@ -40,7 +42,7 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 1)
   ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
@@ -54,6 +56,10 @@ export class AppComponent {
     } else {
       return "bg-info";
     }
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
   }
 }
 
