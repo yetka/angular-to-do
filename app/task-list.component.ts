6086628f0335fb0b1b781cd561c4432ced81f7ea
Guest User <Guest@epicodus-7.local>
2018-01-18 12:31:20 -0800

    add option to make task done

--- app/task-list.component.ts
+++ app/task-list.component.ts
@@ -6,15 +6,13 @@ import { Task } from './task.model';
 @Component({
   selector: 'task-list',
   template:`
-  <select (change)="onChange($event.target.value)">
-    <option value="allTasks">All Tasks</option>
-    <option value="completedTasks">Completed Tasks</option>
-    <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
-  </select>
-
-
+    <select (change)="onChange($event.target.value)">
+      <option value="allTasks">All Tasks</option>
+      <option value="completedTasks">Completed Tasks</option>
+      <option value="incompleteTasks" selected="selected">Incomplete Tasks</option>
+    </select>
     <ul>
-        <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}} {{currentTask.priority}}
+      <li (click)="isDone(currentTask)" *ngFor="let currentTask of childTaskList | completeness:filterByCompleteness">{{currentTask.description}} {{currentTask.priority}}
         <input *ngIf="currentTask.done === true" type="checkbox" checked (click)="toggleDone(currentTask, false)"/>
         <input *ngIf="currentTask.done === false" type="checkbox" (click)="toggleDone(currentTask, true)"/>
         <button (click)="editButtonHasBeenClicked(currentTask)">Edit!</button>
@@ -41,6 +39,10 @@ export class TaskListComponent {
       alert("This task is not done. Better get to work!");
     }
   }
+
+  toggleDone(clickedTask: Task, setCompleteness: boolean) {
+     clickedTask.done = setCompleteness;
+   }
 
   priorityColor(currentTask){
     if (currentTask.priority === 3){
